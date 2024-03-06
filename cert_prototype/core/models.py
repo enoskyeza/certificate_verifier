c08e4b027def4fa4b36b77538ca49d89e34d93f5
enoskyeza <enoskyeza@gmail.com>
2024-03-06 03:36:48 +0300

    Add student and certificate models

--- cert_prototype/core/models.py
+++ cert_prototype/core/models.py
@@ -3,3 +3,13 @@ from django.db import models
 # Create your models here.
 # core/models.py
 
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=10, unique=True)
+    pay_status = models.CharField(max_length=20, choices=[('paid', 'Paid'), ('pending', 'Pending')])
+    course_status = models.CharField(max_length=20, choices=[('complete', 'Complete'), ('enrolling', 'Enrolling')])
+    qr_code = models.ImageField(upload_to='qr_codes/', null=True, blank=True)
+
+class Certificate(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date_of_issuance = models.DateField()
